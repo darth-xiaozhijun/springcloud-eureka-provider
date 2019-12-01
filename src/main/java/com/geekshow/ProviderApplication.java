7b04d0dbbb5e0ffd7c7380ff059d02a96a5fb114
darth-xiaozhijun <15717061273@163.com>
2019-12-01 10:43:55 +0800

    修改名称

--- src/main/java/com/geekshow/ProviderApplication.java
+++ src/main/java/com/geekshow/ProviderApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class EurekaApplication {
+public class ProviderApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication.run(ProviderApplication.class, args);
 	}
 }
